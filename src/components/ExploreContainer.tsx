479fb6c7a5e2fd221c55cc104207b9133a1111cb
khaiky <samanthagabitano015@gmail.com>
2026-03-26 15:23:24 +0800

    adding inline toast

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -1,18 +1,19 @@
 import './ExploreContainer.css';
 import { IonButton, IonHeader, IonContent, IonToolbar, IonTitle, IonPage, IonToast } from '@ionic/react';
-
 interface ContainerProps { }
 
 const ExploreContainer: React.FC<ContainerProps> = () => {
-  return (
+     return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Explore</IonTitle>
+          <IonTitle>Inline Toast</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
-        <IonButton id="open-toast">Open Toast</IonButton>
+      <IonContent className="ion-padding">
+        <IonButton id="open-toast" expand="block">
+          Open
+        </IonButton>
         <p>This toast example uses triggers to automatically open a toast when the button is clicked.</p>
         <IonToast trigger="open-toast" message="This toast will disappear after 5 seconds" duration={5000}></IonToast>
       </IonContent>
